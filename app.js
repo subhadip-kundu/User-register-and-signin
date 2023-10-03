59d4c4fd94e4330ae9de0caf15e30eaf65f94b65
subhadip-kundu <subhadipkundu106@gmail.com>
2023-10-03 17:03:14 +0530

    Cross platform enabled

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ const app = express();
 const authRouter = require('./router/authRoute.js');
 const databaseConnect = require('./config/databaseConfig.js');
 const cookieParser = require('cookie-parser')
+const cors = require('cors');
 
 databaseConnect();
 
 app.use(express.json());
 app.use(cookieParser());
+
+
+//Help to use another(Frontent runs in another port) port access to this code
+app.use(cors({
+    origin: [process.env.CLIENT_URL],
+    credentials:true
+}))
+
+
 app.use('/api/auth',authRouter);
 
 app.use('/', (req,res)=>{
